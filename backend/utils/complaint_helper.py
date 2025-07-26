bc2f8df6c30f5b17b1cee4a000006eb9bc6afb77
Aj1189 <60665588+Aj1189@users.noreply.github.com>
2025-07-27 02:35:40 +0530

    Update complaint_helper.py

--- backend/utils/complaint_helper.py
+++ backend/utils/complaint_helper.py
@@ -5,7 +5,7 @@ def mark_complaint_as_rejected(complaint_id, reason):
     if complaint:
         complaint.status = "Rejected"
         complaint.rejection_reason = reason
-        complaint.image_url = "https://forest-dept-complt-image.s3.us-east-1.amazonaws.com/default/blocked.png"
+        complaint.image_url = "https://20216777-complaints-images.s3.us-east-1.amazonaws.com/complaints/restricted_logo.png"
         db.session.commit()
         return "success"
     else:
